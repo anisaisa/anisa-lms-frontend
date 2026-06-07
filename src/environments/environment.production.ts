a40712c8154506628cac90ce4acbf3a431e25881
Anisa Isa <ai30776@seeu.edu.mk>
2026-06-07 22:29:27 +0200

    Update production API URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,8 @@
+//export const environment = {
+  //production: true,
+  //apiUrl: 'https://localhost:7253/api',
+//};
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7253/api',
+  apiUrl: 'https://anisa-lms-production.up.railway.app/api',
 };
