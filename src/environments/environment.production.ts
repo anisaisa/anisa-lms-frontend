7d6603b9c07b6b4104423afe333ddc507e11fec5
Anisa Isa <ai30776@seeu.edu.mk>
2026-06-07 23:37:37 +0200

    Add serve package for Railway

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,3 @@
-//export const environment = {
-  //production: true,
-  //apiUrl: 'https://localhost:7253/api',
-//};
 export const environment = {
   production: true,
   apiUrl: 'https://anisa-lms-production.up.railway.app/api',
